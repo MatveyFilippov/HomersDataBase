de52b6d8f1ff1aa5150d9853f353eaf2a43d61db
MatveyFilippov <matdufi2004@gmail.com>
2024-12-03 21:15:02 +0300

    rewrite DataBase class to static

--- backend/src/main/java/homer/database/DataBase.java
+++ backend/src/main/java/homer/database/DataBase.java
@@ -15,32 +15,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataBase {
-    public DataBase(String rootDataBaseDirPath, String tableName) {
+
+    public static void setPathToDataBase(String rootDataBaseDirPath, String tableName) {
         FileProcessor.pathToDataBaseRootDir = FileProcessor.join(rootDataBaseDirPath, tableName);
     }
 
-    public void deleteTable() {
+    public static void deleteTable() {
         FileProcessor.deleteRootDir();
     }
 
-    public void createTable(String primaryColumnName, DataTypes primaryColumnDataType) throws IOException {
+    public static void createTable(String primaryColumnName, DataTypes primaryColumnDataType) throws IOException {
         ColumnsProcessor.createPrimaryColumn(primaryColumnName, primaryColumnDataType);
     }
 
-    public void createColumn(String columnName, DataTypes columnDataType, boolean isUnique, boolean isNullPossible) throws IOException {
+    public static void createColumn(String columnName, DataTypes columnDataType, boolean isUnique, boolean isNullPossible) throws IOException {
         ColumnsProcessor.newColumn(columnName, columnDataType, isUnique, isNullPossible);
     }
 
-    public void deleteColumn(String columnName) throws IOException {
+    public static void deleteColumn(String columnName) throws IOException {
         ColumnsProcessor.deleteColumn(columnName);
     }
 
-    public String getPrimaryColumnName() throws IOException, NameNotFoundException, KeyException {
+    public static String getPrimaryColumnName() throws IOException, NameNotFoundException, KeyException {
         UniqueColumn<? extends DataType> column = ColumnsProcessor.getPrimaryColumn();
         return column.columnName;
     }
 
-    public List<String> getColumnNames() throws IOException {
+    public static List<String> getColumnNames() throws IOException {
         List<String> columnNames = new ArrayList<>();
         for (Column<? extends DataType> column : ColumnsProcessor.getColumns()) {
             columnNames.add(column.columnName);
@@ -48,29 +49,29 @@ public class DataBase {
         return columnNames;
     }
 
-    public String getColumnHeader(String columnName) throws IOException, NameNotFoundException {
+    public static String getColumnHeader(String columnName) throws IOException, NameNotFoundException {
         Column<? extends DataType> column = ColumnsProcessor.getColumn(columnName);
         return column.toString();
     }
 
-    public DataTypes getColumnDataType(String columnName) throws NameNotFoundException, IOException {
+    public static DataTypes getColumnDataType(String columnName) throws NameNotFoundException, IOException {
         Column<? extends DataType> column = ColumnsProcessor.getColumn(columnName);
         return column.dataType;
     }
 
-    public <DT extends DataType> DT tryToParseValue(String columnName, String value) throws NameNotFoundException, IOException {
+    public static <DT extends DataType> DT tryToParseValue(String columnName, String value) throws NameNotFoundException, IOException {
         Column<DT> column = ColumnsProcessor.getColumn(columnName);
         return column.dataType.parseValue(value);
     }
 
-    public <DT extends DataType> RecordUniqueID createNewLine(DT primaryKey) throws NameNotFoundException, IOException, KeyException {
+    public static <DT extends DataType> RecordUniqueID createNewLine(DT primaryKey) throws NameNotFoundException, IOException, KeyException {
         UniqueColumn<DT> primaryColumn = ColumnsProcessor.getPrimaryColumn();
         RecordUniqueID key = new RecordUniqueID(primaryKey);
         primaryColumn.writeValue(key, key.toDataType());
         return key;
     }
 
-    public List<RecordUniqueID> getAllRecordsIds() throws NameNotFoundException, IOException, KeyException {
+    public static List<RecordUniqueID> getAllRecordsIds() throws NameNotFoundException, IOException, KeyException {
         List<RecordUniqueID> values = new ArrayList<>();
         UniqueColumn<? extends DataType> primaryColumn = ColumnsProcessor.getPrimaryColumn();
         for (DataType value : primaryColumn.getAllValues()) {
@@ -79,12 +80,12 @@ public class DataBase {
         return values;
     }
 
-    public <DT extends DataType> void writeValue(String columnName, RecordUniqueID recordUniqueID, DT value) throws NameNotFoundException, IOException {
+    public static <DT extends DataType> void writeValue(String columnName, RecordUniqueID recordUniqueID, DT value) throws NameNotFoundException, IOException {
         Column<DT> column = ColumnsProcessor.getColumn(columnName);
         column.writeValue(recordUniqueID, value);
     }
 
-    public <DT extends DataType> DT readValue(String columnName, RecordUniqueID recordUniqueID) throws NameNotFoundException, IOException {
+    public static <DT extends DataType> DT readValue(String columnName, RecordUniqueID recordUniqueID) throws NameNotFoundException, IOException {
         Column<DT> column = ColumnsProcessor.getColumn(columnName);
         return column.readValue(recordUniqueID);
     }
