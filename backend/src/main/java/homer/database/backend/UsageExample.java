784de320059e08e1dcc8366ac5357c935e530448
MatveyFilippov <matdufi2004@gmail.com>
2024-12-03 22:39:45 +0300

    create UsageExample for DataBase

---
+++ backend/src/main/java/homer/database/backend/UsageExample.java
@@ -0,0 +1,33 @@
+package homer.database.backend;
+
+import homer.database.backend.engine.columns.helpers.RecordUniqueID;
+import homer.database.backend.engine.datatypes.NumberType;
+import homer.database.backend.engine.datatypes.helpers.DataTypes;
+
+import javax.naming.NameNotFoundException;
+import java.io.IOException;
+import java.security.KeyException;
+
+public class UsageExample {
+    public static void main(String[] args) throws IOException, NameNotFoundException, KeyException {
+        DataBase.setPathToDataBase("DataBaseExample");
+
+        DataBase.createTable("ID", DataTypes.NUMBER);
+        DataBase.createColumn("Name", DataTypes.STRING, false, true);
+
+        NumberType id1 = new NumberType(1);
+        RecordUniqueID newLineID = DataBase.createNewLine(id1);
+        DataBase.writeValue("Name", newLineID, DataTypes.STRING.parseValue("Homer"));
+
+        for (String columnName : DataBase.getColumnNames()) {
+            System.out.print(DataBase.getColumnHeader(columnName) + "\t");
+        }
+        System.out.println();
+        for (RecordUniqueID lineID : DataBase.getAllRecordsIds()) {
+            for (String columnName : DataBase.getColumnNames()) {
+                System.out.print(DataBase.readValue(columnName, lineID) + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
