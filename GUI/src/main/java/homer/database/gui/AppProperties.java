357cf77c428b452f325be1124043d44b4c639124
MatveyFilippov <matdufi2004@gmail.com>
2025-02-01 23:05:06 +0300

    change GUI path to data dir

--- GUI/src/main/java/homer/database/gui/AppProperties.java
+++ GUI/src/main/java/homer/database/gui/AppProperties.java
@@ -1,9 +1,12 @@
 package homer.database.gui;
 
 import java.io.File;
+import java.nio.file.Paths;
 
 public class AppProperties {
-    public static final String PATH_TO_DATA_DIR = "HomerDataBaseAppData";
+    public static final String PATH_TO_DATA_DIR = Paths.get(
+            System.getProperty("user.home"), ".HomerDataBaseAppData"
+    ).toString();
     public static final String PATH_TO_LOG_FILE = PATH_TO_DATA_DIR + File.separator + "HomerDataBaseApp.log";
     public static final String DB_NAME = "AppMainDataBase";
 }
