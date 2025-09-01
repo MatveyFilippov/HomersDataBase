17f90fb11edeaf8b0a43cac597ebbc50d21728a2
MatveyFilippov <matdufi2004@gmail.com>
2025-09-01 16:56:07 +0300

    create module-info

---
+++ backend/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module homer.database.backend {
+    requires com.google.common;
+
+    exports homer.database.backend;
+    exports homer.database.backend.datatypes;
+    exports homer.database.backend.exceptions;
+}
